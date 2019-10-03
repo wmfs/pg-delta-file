ea144387d8eb3da6349a2818b405977ff3e0f314
Jez Higgins <jez@jezuk.co.uk>
2019-10-03 12:35:10 +0100

    Dry-run tests

--- test/tests.js
+++ test/tests.js
@@ -4,6 +4,7 @@
 
 const HlPgClient = require('@wmfs/hl-pg-client')
 const chai = require('chai')
+chai.use(require('dirty-chai'))
 const expect = chai.expect
 const path = require('path')
 const generateDelta = require('../lib')
@@ -33,246 +34,248 @@ describe('Run the basic usage example', function () {
     })
   })
 
-  describe('pg-delta-file', () => {
-    const tests = [
-      {
-        name: 'single delta',
-        file: 'single-delta.csv',
-        count: 5,
-        info: {
-          totalCount: 5,
-          people: {
-            totalCount: 5
-          }
-        },
-        delta: {
-          namespace: 'springfield',
-          since: '2016-06-03 15:02:38.000000 GMT',
-          actionAliases: {
-            insert: 'i',
-            update: 'u',
-            delete: 'd'
-          },
-          csvExtracts: {
-            people: [
-              73,
-              '$ACTION',
-              '$ROW_NUM',
-              '@social_security_id',
-              '@first_name',
-              '@last_name',
-              '@age'
-            ]
-          }
+  const tests = [
+    {
+      name: 'single delta',
+      file: 'single-delta.csv',
+      count: 5,
+      info: {
+        totalCount: 5,
+        people: {
+          totalCount: 5
         }
       },
-      {
-        name: 'delta with header',
-        file: 'with-header.csv',
-        count: 7,
-        info: {
-          totalCount: 7,
-          '-HEADER-': {
-            totalCount: 1
-          },
-          people: {
-            totalCount: 5
-          },
-          '-FOOTER-': {
-            totalCount: 1
-          }
+      delta: {
+        namespace: 'springfield',
+        since: '2016-06-03 15:02:38.000000 GMT',
+        actionAliases: {
+          insert: 'i',
+          update: 'u',
+          delete: 'd'
         },
-        delta: {
-          namespace: 'springfield',
-          since: '2016-06-03 15:02:38.000000 GMT',
-          actionAliases: {
-            insert: 'i',
-            update: 'u',
-            delete: 'd'
-          },
-          csvExtracts: {
-            '-HEADER-': [
-              'a string',
-              '$ROW_NUM',
-              'another string',
-              999
-            ],
-            people: [
-              73,
-              '$ACTION',
-              '$ROW_NUM',
-              '@social_security_id',
-              '@first_name',
-              '@last_name',
-              '@age'
-            ],
-            '-FOOTER-': [
-              'all done',
-              '$ROW_NUM',
-              0,
-              0
-            ]
-          }
-        }
-      },
-      {
-        name: 'delta for both tables',
-        file: 'multiple-delta.csv',
-        count: 6,
-        info: {
-          totalCount: 6,
-          homes: {
-            totalCount: 3
-          },
-          people: {
-            totalCount: 3
-          }
-        },
-        delta: {
-          namespace: 'springfield', // to be inferred
-          since: '2017-06-02 15:02:38.000000 GMT',
-          actionAliases: {
-            insert: 'i',
-            update: 'u',
-            delete: 'd'
-          },
-          csvExtracts: {
-            homes: [
-              74,
-              '$ACTION',
-              '@address',
-              '@owner_id'
-            ],
-            people: [
-              73,
-              '$ACTION',
-              '@social_security_id',
-              '@first_name',
-              '@last_name',
-              '@age'
-            ]
-          }
-        }
-      },
-      {
-        name: 'delta with transformer',
-        file: 'upper-cased.csv',
-        count: 5,
-        info: {
-          totalCount: 5,
-          people: {
-            totalCount: 5
-          }
-        },
-        delta: {
-          namespace: 'springfield',
-          since: '2016-06-03 15:02:38.000000 GMT',
-          actionAliases: {
-            insert: 'i',
-            update: 'u',
-            delete: 'd'
-          },
-          transformFunction: function (row, callback) {
-            row[4] = row[4].toUpperCase()
-            row[5] = row[5].toUpperCase()
-            callback(null, row)
-          },
-          csvExtracts: {
-            people: [
-              73,
-              '$ACTION',
-              '$ROW_NUM',
-              '@social_security_id',
-              '@first_name',
-              '@last_name',
-              '@age'
-            ]
-          }
-        }
-      },
-      {
-        name: 'delta with filter',
-        file: 'filtered.csv',
-        count: 3,
-        info: {
-          totalCount: 3,
-          people: {
-            totalCount: 3,
-            filteredCount: 2
-          }
-        },
-        delta: {
-          namespace: 'springfield',
-          since: '2016-06-03 15:02:38.000000 GMT',
-          actionAliases: {
-            insert: 'i',
-            update: 'u',
-            delete: 'd'
-          },
-          filterFunction: function (row) {
-            return +row[6] < 60
-          },
-          csvExtracts: {
-            people: [
-              73,
-              '$ACTION',
-              '$ROW_NUM',
-              '@social_security_id',
-              '@first_name',
-              '@last_name',
-              '@age'
-            ]
-          }
-        }
-      },
-      {
-        name: 'delta with filter and transform',
-        file: 'filtered-upper-cased.csv',
-        count: 3,
-        info: {
-          totalCount: 3,
-          people: {
-            totalCount: 3,
-            filteredCount: 2
-          }
-        },
-        delta: {
-          namespace: 'springfield',
-          since: '2016-06-03 15:02:38.000000 GMT',
-          actionAliases: {
-            insert: 'i',
-            update: 'u',
-            delete: 'd'
-          },
-          filterFunction: function (row) {
-            return +row[6] < 60
-          },
-          transformFunction: function (row, callback) {
-            row[4] = row[4].toUpperCase()
-            row[5] = row[5].toUpperCase()
-            callback(null, row)
-          },
-          csvExtracts: {
-            people: [
-              73,
-              '$ACTION',
-              '$ROW_NUM',
-              '@social_security_id',
-              '@first_name',
-              '@last_name',
-              '@age'
-            ]
-          }
+        csvExtracts: {
+          people: [
+            73,
+            '$ACTION',
+            '$ROW_NUM',
+            '@social_security_id',
+            '@first_name',
+            '@last_name',
+            '@age'
+          ]
         }
       }
-    ]
+    },
+    {
+      name: 'delta with header',
+      file: 'with-header.csv',
+      count: 7,
+      info: {
+        totalCount: 7,
+        '-HEADER-': {
+          totalCount: 1
+        },
+        people: {
+          totalCount: 5
+        },
+        '-FOOTER-': {
+          totalCount: 1
+        }
+      },
+      delta: {
+        namespace: 'springfield',
+        since: '2016-06-03 15:02:38.000000 GMT',
+        actionAliases: {
+          insert: 'i',
+          update: 'u',
+          delete: 'd'
+        },
+        csvExtracts: {
+          '-HEADER-': [
+            'a string',
+            '$ROW_NUM',
+            'another string',
+            999
+          ],
+          people: [
+            73,
+            '$ACTION',
+            '$ROW_NUM',
+            '@social_security_id',
+            '@first_name',
+            '@last_name',
+            '@age'
+          ],
+          '-FOOTER-': [
+            'all done',
+            '$ROW_NUM',
+            0,
+            0
+          ]
+        }
+      }
+    },
+    {
+      name: 'delta for both tables',
+      file: 'multiple-delta.csv',
+      count: 6,
+      info: {
+        totalCount: 6,
+        homes: {
+          totalCount: 3
+        },
+        people: {
+          totalCount: 3
+        }
+      },
+      delta: {
+        namespace: 'springfield', // to be inferred
+        since: '2017-06-02 15:02:38.000000 GMT',
+        actionAliases: {
+          insert: 'i',
+          update: 'u',
+          delete: 'd'
+        },
+        csvExtracts: {
+          homes: [
+            74,
+            '$ACTION',
+            '@address',
+            '@owner_id'
+          ],
+          people: [
+            73,
+            '$ACTION',
+            '@social_security_id',
+            '@first_name',
+            '@last_name',
+            '@age'
+          ]
+        }
+      }
+    },
+    {
+      name: 'delta with transformer',
+      file: 'upper-cased.csv',
+      count: 5,
+      info: {
+        totalCount: 5,
+        people: {
+          totalCount: 5
+        }
+      },
+      delta: {
+        namespace: 'springfield',
+        since: '2016-06-03 15:02:38.000000 GMT',
+        actionAliases: {
+          insert: 'i',
+          update: 'u',
+          delete: 'd'
+        },
+        transformFunction: function (row, callback) {
+          row[4] = row[4].toUpperCase()
+          row[5] = row[5].toUpperCase()
+          callback(null, row)
+        },
+        csvExtracts: {
+          people: [
+            73,
+            '$ACTION',
+            '$ROW_NUM',
+            '@social_security_id',
+            '@first_name',
+            '@last_name',
+            '@age'
+          ]
+        }
+      }
+    },
+    {
+      name: 'delta with filter',
+      file: 'filtered.csv',
+      count: 3,
+      info: {
+        totalCount: 3,
+        people: {
+          totalCount: 3,
+          filteredCount: 2
+        }
+      },
+      delta: {
+        namespace: 'springfield',
+        since: '2016-06-03 15:02:38.000000 GMT',
+        actionAliases: {
+          insert: 'i',
+          update: 'u',
+          delete: 'd'
+        },
+        filterFunction: function (row) {
+          return +row[6] < 60
+        },
+        csvExtracts: {
+          people: [
+            73,
+            '$ACTION',
+            '$ROW_NUM',
+            '@social_security_id',
+            '@first_name',
+            '@last_name',
+            '@age'
+          ]
+        }
+      }
+    },
+    {
+      name: 'delta with filter and transform',
+      file: 'filtered-upper-cased.csv',
+      count: 3,
+      info: {
+        totalCount: 3,
+        people: {
+          totalCount: 3,
+          filteredCount: 2
+        }
+      },
+      delta: {
+        namespace: 'springfield',
+        since: '2016-06-03 15:02:38.000000 GMT',
+        actionAliases: {
+          insert: 'i',
+          update: 'u',
+          delete: 'd'
+        },
+        filterFunction: function (row) {
+          return +row[6] < 60
+        },
+        transformFunction: function (row, callback) {
+          row[4] = row[4].toUpperCase()
+          row[5] = row[5].toUpperCase()
+          callback(null, row)
+        },
+        csvExtracts: {
+          people: [
+            73,
+            '$ACTION',
+            '$ROW_NUM',
+            '@social_security_id',
+            '@first_name',
+            '@last_name',
+            '@age'
+          ]
+        }
+      }
+    }
+  ]
 
+  describe('delta-file generation', () => {
     for (const test of tests) {
       it(test.name, async () => {
         const outputFile = path.resolve(__dirname, 'output', test.file)
         const expectedFile = path.resolve(__dirname, 'fixtures', 'expected', test.file)
+
         test.delta.client = client
         test.delta.outputFilepath = outputFile
+        test.delta.dryrun = false
 
         const info = await generateDelta(
           test.delta
@@ -285,6 +288,28 @@ describe('Run the basic usage example', function () {
         const expected = readRecords(expectedFile)
 
         expect(output).to.eql(expected)
+      })
+    }
+  })
+
+  describe('delta dry-run', () => {
+    for (const test of tests) {
+      it(test.name, async () => {
+        const outputFile = path.resolve(__dirname, 'output', test.file)
+        fs.unlinkSync(outputFile)
+
+        test.delta.client = client
+        test.delta.outputFilepath = outputFile
+        test.delta.dryrun = true
+
+        const info = await generateDelta(
+          test.delta
+        )
+
+        expect(info.totalCount).to.eql(test.count)
+        expect(info).to.eql(test.info)
+
+        expect(fs.existsSync(outputFile)).to.be.false()
       })
     }
   })
